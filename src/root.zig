cd4780db9d9c08d2ddf1d8ef208be98f5e58e5ab
Mathieu Cayeux <mathieucayeux@gmail.com>
2025-09-05 11:34:07 +0200

    feat: add and impl stdarg

--- src/root.zig
+++ src/root.zig
@@ -6,6 +6,7 @@ pub const errno = @import("errno.zig");
 pub const float = @import("float.zig");
 pub const limits = @import("limits.zig");
 pub const math = @import("math.zig");
+pub const stdarg = @import("stdarg.zig");
 pub const stddef = @import("stddef.zig");
 
 const c = struct {
